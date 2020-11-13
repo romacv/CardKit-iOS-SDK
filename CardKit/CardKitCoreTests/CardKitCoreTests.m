dd36f474c4be4e7796d248ec08a4116cee478edc
Alex Korotkov <alexkorotkovh@gmail.com>
2020-11-13 16:30:11 +0300

    Add tests

--- CardKit/CardKitCoreTests/CardKitCoreTests.m
+++ CardKit/CardKitCoreTests/CardKitCoreTests.m
@@ -16,9 +16,18 @@
 
 @end
 
-@implementation CardKitCoreTests
+@implementation CardKitCoreTests {
+    CKCCardParams *cardParams;
+}
 
 - (void)setUp {
+    cardParams = [[CKCCardParams alloc] init];
+    cardParams.cardholder=@"Korotkov Alex";
+    cardParams.expiryMMYY=@"1222";
+    cardParams.pan=@"5536913776755304";
+    cardParams.cvc = @"123";
+    cardParams.mdOrder = @"mdorder";
+    cardParams.pubKey = @"-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoIITqh9xlGx4tWA+aucb0V0YuFC9aXzJb0epdioSkq3qzNdRSZIxe/dHqcbMN2SyhzvN6MRVl3xyjGAV+lwk8poD4BRW3VwPUkT8xG/P/YLzi5N8lY6ILlfw6WCtRPK5bKGGnERcX5dqL60LhOPRDSYT5NHbbp/J2eFWyLigdU9Sq7jvz9ixOLh6xD7pgNgHtnOJ3Cw0Gqy03r3+m3+CBZwrzcp7ZFs41bit7/t1nIqgx78BCTPugap88Gs+8ZjdfDvuDM+/3EwwK0UVTj0SQOv0E5KcEHENL9QQg3ujmEi+zAavulPqXH5907q21lwQeemzkTJH4o2RCCVeYO+YrQIDAQAB-----END PUBLIC KEY-----";
 }
 
 - (void)tearDown {
@@ -41,19 +50,177 @@
 }
 
 - (void)testGenerateTokenWithCard {
-    CKCCardParams *cardParams = [[CKCCardParams alloc] init];
-    cardParams.cardholder=@"Korotkov Alex";
-    cardParams.expiryMMYY=@"1222";
-    cardParams.pan=@"5536913776755304";
-    cardParams.cvc = @"123";
-    cardParams.mdOrder = @"mdorder";
-    cardParams.pubKey = @"-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAoIITqh9xlGx4tWA+aucb0V0YuFC9aXzJb0epdioSkq3qzNdRSZIxe/dHqcbMN2SyhzvN6MRVl3xyjGAV+lwk8poD4BRW3VwPUkT8xG/P/YLzi5N8lY6ILlfw6WCtRPK5bKGGnERcX5dqL60LhOPRDSYT5NHbbp/J2eFWyLigdU9Sq7jvz9ixOLh6xD7pgNgHtnOJ3Cw0Gqy03r3+m3+CBZwrzcp7ZFs41bit7/t1nIqgx78BCTPugap88Gs+8ZjdfDvuDM+/3EwwK0UVTj0SQOv0E5KcEHENL9QQg3ujmEi+zAavulPqXH5907q21lwQeemzkTJH4o2RCCVeYO+YrQIDAQAB-----END PUBLIC KEY-----";
-
-    CKCTokenResult *resForTesting = [[CKCTokenResult alloc] init];
-    
     CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
-    
+
     XCTAssertNotNil(res.token, @"pointer:%p", res.token);
+}
+
+- (void)testGenerateTokenWithCardWithoutCardholder {
+    cardParams.cardholder=@"";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldCardholder, @"error": CKCErrorRequired};
+    
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithNilCardholder {
+    cardParams.cardholder=nil;
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldCardholder, @"error": CKCErrorRequired};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithNumbersCardholder {
+    cardParams.cardholder=@"1223324";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldCardholder, @"error": CKCErrorInvalidFormat};
+    
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithoutExpiryMMYY {
+    cardParams.expiryMMYY=@"";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldExpiryMMYY, @"error": CKCErrorRequired};
+    
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithNilExpiryMMYY {
+    cardParams.expiryMMYY=nil;
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldExpiryMMYY, @"error": CKCErrorRequired};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithInvalidExpiryMMYY {
+    cardParams.expiryMMYY=@"222/2";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldExpiryMMYY, @"error": CKCErrorInvalidFormat};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithNilPan {
+    cardParams.pan=nil;
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldPan, @"error": CKCErrorRequired};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithoutPan {
+    cardParams.pan=@"";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldPan, @"error": CKCErrorRequired};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithLettersPan {
+    cardParams.pan=@"qwertyuiopasdfghjk";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldPan, @"error": CKCErrorInvalidFormat};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithSmallPan {
+    cardParams.pan=@"1232131231321";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldPan, @"error": CKCErrorInvalidLength};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithInvalidPan {
+    cardParams.pan=@"123456789012345678";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldPan, @"error": CKCErrorInvalidFormat};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithoutCVC {
+    cardParams.cvc=@"";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldCVC, @"error": CKCErrorRequired};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardNilCVC {
+    cardParams.cvc=nil;
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldCVC, @"error": CKCErrorRequired};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithInvalidCVC {
+    cardParams.cvc=@"adsa";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldCVC, @"error": CKCErrorInvalidFormat};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithLargeCVC {
+    cardParams.cvc=@"1234";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldCVC, @"error": CKCErrorInvalidFormat};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithShortCVC {
+    cardParams.cvc=@"14";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldCVC, @"error": CKCErrorInvalidFormat};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithInvalidPubKey {
+    cardParams.pubKey = @"adadskmaaklasd";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldPubKey, @"error": CKCErrorInvalid};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithoutPubKey {
+    cardParams.pubKey =@"";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldPubKey, @"error": CKCErrorRequired};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithNilPubKey {
+    cardParams.pubKey = nil;
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldPubKey, @"error": CKCErrorRequired};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithoutMdOrder {
+    cardParams.mdOrder =@"";
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldMdOrder, @"error": CKCErrorRequired};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
+}
+
+- (void)testGenerateTokenWithCardWithNilMdOrder {
+    cardParams.mdOrder = nil;
+    CKCTokenResult *res = [CKCToken generateWithCard:cardParams];
+    NSDictionary *expectedResponse = @{@"field": CKCFieldMdOrder, @"error": CKCErrorRequired};
+
+    XCTAssertEqualObjects(res.errors[0], expectedResponse);
 }
 
 - (void)testReturnPubKeyFromJSONString {
