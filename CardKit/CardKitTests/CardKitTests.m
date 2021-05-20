9e1aadf6f46db9e132674952743d73c56fc3e727
Alex Korotkov <alexkorotkovh@gmail.com>
2021-05-20 16:37:20 +0300

    Add tests for CardKValidation

--- CardKit/CardKitTests/CardKitTests.m
+++ CardKit/CardKitTests/CardKitTests.m
@@ -7,7 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
-
+#import "CardKValidation.h"
 @interface CardKitTests : XCTestCase
 
 @end
@@ -15,23 +15,33 @@
 @implementation CardKitTests
 
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testIsValidateSecureCodeWhenStringIsShort {
+  XCTAssertFalse([CardKValidation isValidSecureCode:@"1"]);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testIsValidateSecureCodeWhenStringIsCorrect {
+  XCTAssertTrue([CardKValidation isValidSecureCode:@"123"]);
+}
+
+- (void)testIsValidateSecureCodeWhenStringIsLong {
+  XCTAssertFalse([CardKValidation isValidSecureCode:@"1234"]);
+}
+
+- (void)testIsValidateSecureCodeWhenStringHasLetters {
+  XCTAssertFalse([CardKValidation isValidSecureCode:@"1a2"]);
+}
+
+- (void)testAllDigitsInStringWhenStringHasLetters {
+  XCTAssertFalse([CardKValidation isValidSecureCode:@"1a2"]);
+}
+
+- (void)testAllDigitsInString {
+  XCTAssertTrue([CardKValidation isValidSecureCode:@"123"]);
 }
 
 @end
